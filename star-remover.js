1928e2ed6602a6a25f2e2e22456f72d421fb7a02
gemoxley <gmoxley@dons.usfca.edu>
2024-05-03 22:12:03 -0700

    capital

--- star-remover.js
+++ star-remover.js
@@ -7,7 +7,7 @@ let index = 0;
 let typingSpeed = 50; 
 
 function preload() {
-  customFont = loadFont('path/to/your/font.ttf');
+  customFont = loadFont('JosefinSlab-VariableFont_wght.tff');
   img = loadImage('Yellow Star.png');
 }
 function setup() {
@@ -19,11 +19,24 @@ function resized() {
 }
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  textFont(customFont);
+  textSize(60);
   background(0, 0, 0, 0);
 }
 function draw() {
   image(img, 400, 400, 200, 200);
+  text(typedMessage, 50, 50);
+  typedMessage = "In order to best navigate this website, click on solid pink words to proceed to the next page when you're finished reading.";
+  index = 0;
+  setTimeout(typeWriter, typingSpeed);
 }
 function mousePressed() {
     remove(); 
+}
+function typeWriter() {
+  typedMessage += message.charAt(index);
+  index++;
+  if (index < message.length) {
+    setTimeout(typeWriter, typingSpeed);
+  }
 }
