cc93464b2dd73df53fa8c91c24605e67e6a93ea6
gemoxley <gmoxley@dons.usfca.edu>
2024-05-03 21:22:18 -0700

    Index Time

--- star-remover.js
+++ star-remover.js
@@ -5,15 +5,20 @@ function preload() {
   img = loadImage('Yellow Star.png');
 }
 function setup() {
+  let canvas = createCanvas(400, 400);
+  canvas.parent('canvas-container');
+}
+function resized() {
   createCanvas(windowWidth, windowHeight);
 }
-function draw() {
-  background(220);
-  image(img, 0, 0);
-  if (showImage) {
-    image(img, 0, 0, width, height);
-  }
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(0, 0, 0, 0);
 }
+function draw() {
+    image(img, 0, 0, width, height);
+}
+
 function mousePressed() {
-    showImage = !showImage;
+    remove(); 
 }
