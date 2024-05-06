ad59e9ed5eab1d07e8f3d137c8e368a22d19cfd4
gemoxley <gmoxley@dons.usfca.edu>
2024-05-06 15:07:13 -0700

    It's going

--- star-remover.js
+++ star-remover.js
@@ -5,28 +5,40 @@ let message = "Hello, world!";
 let typedMessage = "";
 let index = 0;
 let typingSpeed = 50;
+let rotationAngle = 0;
+
 function preload() {
   customFont = loadFont('JosefinSlab-VariableFont_wght-webfont.woff');
   img = loadImage('Yellow Star.png');
 }
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   textFont(customFont);
   textSize(60);
   textStyle(BOLD);
 }
+
 function draw() {
   background(0, 0, 0, 0);
-  image(img, width/1.5, height/1.5, 200, 200);
+  imageMode(CENTER);
+  push();
+  translate(width/1.5, height/1.5);
+  rotationAngle += 0.1; // Increment rotation angle
+  rotate(rotationAngle);
+  image(img, 0, 0, 200, 200);
+  pop();
   fill('white');
   text(typedMessage, width/1.5, height/1.5);
 }
+
 function mousePressed() {
-  if (mouseX > width/1.5 && mouseX < width/1.5 + 200 && mouseY > height/1.5 && mouseY < height/1.5 + 200) {
+  if (dist(mouseX, mouseY, width/1.5, height/1.5) < 100) {
     typedMessage = "";
     typeWriter();
   }
 }
+
 function typeWriter() {
   typedMessage += message.charAt(index);
   index++;
