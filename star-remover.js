d38ffe7d815be300f0f2854be1f53d0514b01831
gemoxley <gmoxley@dons.usfca.edu>
2024-05-06 13:44:30 -0700

    JS

--- star-remover.js
+++ star-remover.js
@@ -4,26 +4,33 @@ let customFont;
 let message = "Hello, world!";
 let typedMessage = "";
 let index = 0;
-let typingSpeed = 50; 
+let typingSpeed = 50;
 
 function preload() {
-  customFont = loadFont('JosefinSlab-VariableFont_wght.tff');
+  customFont = loadFont('JosefinSlab-VariableFont_wght-webfont.woff');
   img = loadImage('Yellow Star.png');
 }
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
   textFont(customFont);
   textSize(60);
 }
+
 function draw() {
   background(0, 0, 0, 0);
-  image(img, 400, 400, 200, 200);
-  text(typedMessage, 200, 200);
+  image(img, width/1.5, height/1.5, 200, 200);
+  fill('white');
+  text(typedMessage, width/1.5, height/1.5);
 }
+
 function mousePressed() {
-  typedMessage = "";
-  typeWriter();
+  if (mouseX > width/1.5 && mouseX < width/1.5 + 200 && mouseY > height/1.5 && mouseY < height/1.5 + 200) {
+    typedMessage = "";
+    typeWriter();
+  }
 }
+
 function typeWriter() {
   typedMessage += message.charAt(index);
   index++;
