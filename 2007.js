0c1ed34ab1d016ca674453298fb8ed6a1b0d55e9
gemoxley <gmoxley@dons.usfca.edu>
2024-05-07 10:57:09 -0700

    2007

--- 2007.js
+++ 2007.js
@@ -1,36 +1,44 @@
 let img;
+let img2; 
 let showImage = true;
 let customFont;
-let message = "Circa 2007- I wore a bicycle helmet CONSTANTLY and refused to remove it.";
+let message = "Boston Pride 2023";
 let typedMessage = "";
 let index = 0;
 let typingSpeed = 50;
 let rotationAngle = 0;
 
 function preload() {
-  customFont = loadFont('JosefinSlab-VariableFont_wght-webfont.woff');
-  img = loadImage('Kid.png');
+  customFont = loadFont('JosefinSlab-Bold.otf');
+  img = loadImage('Boston-Star.png');
+  img2 = loadImage('Black_Paper 1.png')
 }
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.position(0, 0); 
+  canvas.style('z-index', '-1'); 
   textFont(customFont);
   textSize(60);
   textStyle(BOLD);
+
 }
 function draw() {
-  background(0, 0, 0, 0);
+  imageMode(CORNER);
+  background(img2);
   imageMode(CENTER);
   push();
-  translate(width/1.5, height/1.5);
-  rotationAngle += 0.1;
+  translate(width/1.5, height/2.2);
+  rotationAngle += 0.02;
   rotate(rotationAngle);
-  image(img, 0, 0, 200, 200);
+  image(img, 0, 0, 500, 500);
   pop();
   fill('white');
-  text(typedMessage, width/1.9, height/1.9);
+  textWidth(500); 
+  text(typedMessage, 900, height/4, 500);
+  textWrap(WORD); 
 }
 function mousePressed() {
-  if (dist(mouseX, mouseY, width/1.5, height/1.5) < 100) {
+  if (dist(mouseX, mouseY, width/1.5, height/2.2) < 100) {
     typedMessage = "";
     typeWriter();
   }
