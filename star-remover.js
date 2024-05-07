d4d1cc1b9220e55ea228aa173c61b2792d48dc41
gemoxley <gmoxley@dons.usfca.edu>
2024-05-07 10:34:41 -0700

    WOTKINGH

--- star-remover.js
+++ star-remover.js
@@ -1,4 +1,5 @@
 let img;
+let img2; 
 let showImage = true;
 let customFont;
 let message = "To best navigate this site, click on blue links to go backwards and pink ones to go forward.";
@@ -10,24 +11,31 @@ let rotationAngle = 0;
 function preload() {
   customFont = loadFont('JosefinSlab-VariableFont_wght-webfont.woff');
   img = loadImage('Yellow Star.png');
+  img2 = loadImage('Black_Paper 1.png')
 }
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.position(0, 0); 
+  canvas.style('z-index', '-1'); 
   textFont(customFont);
   textSize(60);
   textStyle(BOLD);
+
 }
 function draw() {
-  background(0, 0, 0, 0);
+  imageMode(CORNER);
+  background(img2);
   imageMode(CENTER);
   push();
   translate(width/1.5, height/1.5);
-  rotationAngle += 0.1;
+  rotationAngle += 0.01;
   rotate(rotationAngle);
   image(img, 0, 0, 200, 200);
   pop();
-  fill('white');
-  text(typedMessage, width/1.9, height/1.9);
+  fill('red');
+  textWidth(500); 
+  text(typedMessage, 50, height/2, 500);
+  textWrap(WORD); 
 }
 function mousePressed() {
   if (dist(mouseX, mouseY, width/1.5, height/1.5) < 100) {
