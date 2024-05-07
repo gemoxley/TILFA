d11b4d76e6558bc96dbace92c929f7548a0791bc
gemoxley <gmoxley@dons.usfca.edu>
2024-05-07 09:45:41 -0700

    Try?

--- star-remover.js
+++ star-remover.js
@@ -1,7 +1,7 @@
 let img;
 let showImage = true;
 let customFont;
-let message = "Hello, world!";
+let message = "To best navigate this site, click on blue links to go backwards and pink ones to go forward.";
 let typedMessage = "";
 let index = 0;
 let typingSpeed = 50;
@@ -11,38 +11,51 @@ function preload() {
   customFont = loadFont('JosefinSlab-VariableFont_wght-webfont.woff');
   img = loadImage('Yellow Star.png');
 }
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
   textFont(customFont);
   textSize(60);
   textStyle(BOLD);
 }
-
 function draw() {
   background(0, 0, 0, 0);
   imageMode(CENTER);
   push();
-  translate(width/1.5, height/1.5);
-  rotationAngle += 0.1; // Increment rotation angle
+  translate(width / 1.5, height / 1.5);
+  rotationAngle += 0.1;
   rotate(rotationAngle);
   image(img, 0, 0, 200, 200);
   pop();
   fill('white');
-  text(typedMessage, width/1.5, height/1.5);
+  wrapText(typedMessage, width / 1.9, height / 1.9, width / 3, 50);
 }
-
 function mousePressed() {
-  if (dist(mouseX, mouseY, width/1.5, height/1.5) < 100) {
+  if (dist(mouseX, mouseY, width / 1.5, height / 1.5) < 100) {
     typedMessage = "";
+    index = 0;
     typeWriter();
   }
 }
-
 function typeWriter() {
   typedMessage += message.charAt(index);
   index++;
   if (index < message.length) {
     setTimeout(typeWriter, typingSpeed);
   }
+}
+function wrapText(text, x, y, maxWidth, lineHeight) {
+  let words = text.split(' ');
+  let line = '';
+  for (let i = 0; i < words.length; i++) {
+    let testLine = line + words[i] + ' ';
+    let testWidth = textWidth(testLine);
+    if (testWidth > maxWidth && i > 0) {
+      text(line, x, y);
+      line = words[i] + ' ';
+      y += lineHeight;
+    } else {
+      line = testLine;
+    }
+  }
+  text(line, x, y);
 }
