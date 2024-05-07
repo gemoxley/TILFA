4b3493a1edbb97d485f6e5a985df12b282620674
gemoxley <gmoxley@dons.usfca.edu>
2024-05-07 11:25:08 -0700

    Shirt

--- chest.js
+++ chest.js
@@ -2,7 +2,7 @@ let img;
 let img2; 
 let showImage = true;
 let customFont;
-let message = "An image of my chest area a week after gender-affirming top surgery. The chest area is yellow, purple, and bruised- and I couldn't be more thrilled with it.";
+let message = "05/26/2023: An image of my chest area a week after gender-affirming top surgery.";
 let typedMessage = "";
 let index = 0;
 let typingSpeed = 50;
@@ -27,18 +27,18 @@ function draw() {
   background(img2);
   imageMode(CENTER);
   push();
-  translate(width/1.5, height/2.2);
+  translate(width/1.3, height/1.8);
   rotationAngle += 0.02;
   rotate(rotationAngle);
   image(img, 0, 0, 300, 300);
   pop();
   fill('white');
   textWidth(500); 
-  text(typedMessage, 900, height/4, 500);
+  text(typedMessage, 50, height/3, 500);
   textWrap(WORD); 
 }
 function mousePressed() {
-  if (dist(mouseX, mouseY, width/1.5, height/2.2) < 100) {
+  if (dist(mouseX, mouseY, width/1.3, height/1.8) < 100) {
     typedMessage = "";
     typeWriter();
   }
