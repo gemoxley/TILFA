2c047f812310386aaf9d0cb50b9e1ecc74dfa04a
gemoxley <gmoxley@dons.usfca.edu>
2024-05-08 20:01:47 -0700

    Sure

--- sure.js
+++ sure.js
@@ -1,11 +1,12 @@
 //Thank you to p5.js user @zhiying0819 for the base of the code!
 
 const word = "Sure.";
-const fSize = 130; 
+const fSize = 200; 
 
 let myFont;
 let points;
 let wiggle = 1;
+let pointColors = [];
 
 function preload(){
   myFont = loadFont("BethEllen-Regular.ttf");
@@ -16,16 +17,20 @@ function setup() {
   canvas.position(0, 0); 
   canvas.style('z-index', '-1'); 
   points = myFont.textToPoints(word, 0, 0, fSize);
+  for (let i = 0; i < points.length; i++) {
+    pointColors[i] = random(["#FFA7F6", "#FFFFFF", "#44C7FF"]);
+    //Make all the points STAY the color they're supposed to be instead of flashing colors
+  }
 }
 function draw() {
-    imageMode(CORNER);
-    translate(width/1.5, height/1.5);
-    background(img);
-    imageMode(CENTER);
+  imageMode(CORNER);
+  translate(width/1.5, height/1.5);
+  background(img);
+  imageMode(CENTER);
   for (let i = 0; i < points.length; i++) {
     let x = points[i].x + random(-wiggle, wiggle);
     let y = points[i].y + random(-wiggle, wiggle);
-    fill(255);
+    fill(pointColors[i]);
     ellipse(x, y, 5);
   }
 }
